5b9c4d1716711ab7d98148c969a8a5b03bd7af3a
dmxiaoshen <dmxiaoshen@163.com>
2021-09-15 17:47:37 +0800

    Site updated: 2021-09-15 17:47:37

---
+++ js/baidu_analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?84405cc8993d00d19f760d6fa7ec02ca";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
